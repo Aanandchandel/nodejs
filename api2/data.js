4b1fcc314b8c41d987bc58342f6b8700ab6bd754
Aanand <aanandchandel2@gmail.com>
2023-09-21 18:00:36 +0530

    second api

---
+++ api2/data.js
@@ -0,0 +1,11 @@
+const data =[
+ 
+    {name:'Anand ',age: '22',email:'aanandchandel2@gmail.com'},
+    {name:'vishal',age: '22',email:'vishal2@gmail.com'},
+    {name:'yogi ',age: '22',email:'yogi2@gmail.com'},
+    {name:'poonam',age: '22',email:'poonam2@gmail.com'}
+]
+
+
+
+module.exports=data
---
+++ api2/index.js
@@ -0,0 +1,8 @@
+const data =require('./data'); 
+const http=require('http');
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
+
+}).listen(8000);
